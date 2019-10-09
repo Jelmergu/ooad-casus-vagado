2ed48568c901c4fd35d91e86c06aaadba421b432
Jelmergu <github@jelmerwijnja.nl>
2019-10-09 10:05:34 +0200

    Initial Commit
    Signed-off-by: Jelmergu <github@jelmerwijnja.nl>

---
+++ src/main/java/nl/jelmerwijnja/oose/ooad/vagado/App.java
@@ -0,0 +1,13 @@
+package nl.jelmerwijnja.oose.ooad.vagado;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main(String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
